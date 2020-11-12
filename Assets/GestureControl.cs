a203728b6831d10822d443d026e54b04a9cfe9d0
WantingDU <du13550196041@gmail.com>
2020-11-12 18:24:11 +0100

    Initial commit

---
+++ Assets/GestureControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureControl : MonoBehaviour
+{
+
+    private float rotationX;
+    private float scaleRotationX = 5f;
+
+    private float positionX;
+    private float positionY;
+    private float scalePosiontX = 10f;
+    private float scalePosiontY = 10f;
+
+    private float positionW;
+    private float scaleModel = 30f;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1))  //pressing right button of mouse to change position of model and modifying wheel to change model scale
+        {
+           
+            positionX = Input.GetAxis("Mouse X") * scalePosiontX;
+            positionY = Input.GetAxis("Mouse Y") * scalePosiontY;
+            positionW = Input.GetAxis("Mouse ScrollWheel") * scaleModel;
+            transform.position += new Vector3(positionX, positionY, 0f);
+            transform.localScale += new Vector3(positionW, positionW, positionW);
+        }
+        else if (Input.GetMouseButton(2))   //pressing middle button of mouse to change rotation on Y axis of model
+        {
+            rotationX = -Input.GetAxis("Mouse X") * scaleRotationX;
+            transform.Rotate(0f, rotationX, 0f, Space.World);
+        }
+    }
+
+
+}
---
+++ Assets/ModelToBody25.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelToBody25 : MonoBehaviour
+{
+    //--------------------- PILS ------------------------------//
+    public List<Transform> poseStandard;
+    public GameObject ball;
+    private GameObject[] Myballs= new GameObject[10];
+    private MeshRenderer[] renderballs= new MeshRenderer[10];
+    //--------------------- PILS ------------------------------/
+    private void Awake()
+    {
+        ball = Resources.Load<GameObject>("Sphere") as GameObject;
+        for (int i = 0; i < 10; i++)
+        {
+                Myballs[i]=Instantiate(ball, poseStandard[EvaluateAngle.OP_anglePoints[i, 1]].transform);
+                renderballs[i] = Myballs[i].GetComponent<MeshRenderer>();
+                renderballs[i].enabled = false;
+        }
+        
+    }
+    
+    private void Update()
+    {
+        for (int i = 0; i < 10; i++) {
+            if (EvaluateAngle.PointScore[i] == 0)
+            {
+                renderballs[i].enabled = true;
+                renderballs[i].material.color = Color.gray;
+            }
+            else if (EvaluateAngle.PointScore[i] < 0.7)
+            {
+                renderballs[i].enabled = true;
+                renderballs[i].material.color = new Color(1 - EvaluateAngle.PointScore[i], 0, 0, 1);
+            }
+            else
+            {
+                renderballs[i].enabled = false;
+            }   
+        } 
+
+    }
+}
