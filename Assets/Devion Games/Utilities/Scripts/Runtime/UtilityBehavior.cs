55a7d542252573594a4431b0b340edac39204721
RanChen <ran.chen@insa-lyon.fr>
2020-12-10 20:02:30 +0100

    log in test

---
+++ Assets/Devion Games/Utilities/Scripts/Runtime/UtilityBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DevionGames
+{
+    public class UtilityBehavior : MonoBehaviour
+    {
+        public void QuitApplication() {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+
+        public void LoadScene(string scene) {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+        }
+    }
+}
