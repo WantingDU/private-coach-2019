9663a9bd4cb369b051d3191b07496eb4e6599653
CHEN Ran <ran.chen@insa-lyon.fr>
2020-12-24 18:13:52 +0100

    choose sport

---
+++ Assets/Scripts/ChooseSport.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseSport : MonoBehaviour
+{
+    public void Pushup()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
