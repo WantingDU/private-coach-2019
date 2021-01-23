005173bf729eefd0b7fdad406d9f8d99ac32886a
WantingDU <du13550196041@gmail.com>
2021-01-23 23:11:35 +0100

    Update AutoHelpText.cs

--- Assets/Scripts/AutoHelpText.cs
+++ Assets/Scripts/AutoHelpText.cs
@@ -6,7 +6,7 @@ public class AutoHelpText : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.gameObject.GetComponent<Text>().text = "<size=60>Hello" + StaticItems.username+" !</size>" +
+        this.gameObject.GetComponent<Text>().text = "<size=60>Hello " + StaticItems.username+"!</size>" +
                                                   "\nSwipe the screen to change the screen" +
                                                   "\nPress or touch the outside area to close windows";
     }
